7a128d778bd1ac091ceb0e19820b47a853b6fc93
Igor Avelino <igoravelino96@gmail.com>
2023-06-20 15:53:22 -0300

    How use text

--- how_to_use.py
+++ how_to_use.py
@@ -17,10 +17,13 @@ print(art)
 print(
 f"""
 \033[34m[i]\033[33m [[ USAGE: CHOOSE THE PROGRAM YOU WANT TO USE AND COMPLETE THE ARGUMENTS ]]\033[0;0;0m
-{'-'*100}
+
+\033[35;4mPS:\033[0;0;0m \033[34;4mDont forget the\033[0;0;0m \033[33m pip install -r requirements.txt\033[0;0;0m
+
+\033[32m{'-'*100}
 
 \033[32m[+]\033[35m Port Scan:\033[0;0;0m \033[34mpython port_scan.py\033[0;0;0m \033[34m[ \033[0;0;0m\033[33mHOST\033[0;0;0m\033[34m ] [ \033[33mOptional: TIMEOUT\033[0;0;0m\033[34m ] \033[0;0;0m
-    \033[34m[i]\033[0;0;0m The Port Scanning is using the TCP connection (3-Handshake), so it's not discrete but very fast...
+    \033[34m[i]\033[0;0;0m The Port Scanning is using the TCP connection, so it's not discrete but very fast...
 
 \033[32m[+]\033[35m Email/Phone Web Crawler:\033[0;0;0m \033[34mpython web_crawler.py\033[0;0;0m \033[34m[ \033[0;0;0m\033[33mDOMAIN\033[0;0;0m\033[34m ]\033[0;0;0m
     \033[34m[i]\033[0;0;0m The WebCrawler is a program that looks for phone numbers and emails address into a web page, after done it goes to the next link (href tag) list into the source code of HTML...
